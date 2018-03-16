28aa768cbb7a62847bfc74523aacf4ff262b4833
BartMiki <bartmiki97@gmail.com>
2018-03-16 22:55:09 +0100

    Code cleanup

---
+++ EF6_CodeFirst_Exercise/Classification.cs
@@ -0,0 +1,9 @@
+﻿namespace EF6_CodeFirst_Exercise
+{
+    public enum Classification : byte
+    {
+        Silver = 1,
+        Gold,
+        Platinium
+    }
+}
---
+++ EF6_CodeFirst_Exercise/VidzyContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace EF6_CodeFirst_Exercise
+{
+    public class VidzyContext : DbContext
+    {
+        public VidzyContext() : base("DefaultConnection") { }
+
+        public DbSet<Video> Videos { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
